32d37fc8156b07380a6fc200995755016052d3bd
Peter <sqiu@ualberta.ca>
2023-02-10 09:57:05 -0700

    fingers crossed it's all finished

--- httpclient.py
+++ httpclient.py
@@ -33,7 +33,18 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    
+    def get_host_port(self,url):
+        # get the host and the port from the url
+        parsed_url = urllib.parse.urlparse(url)
+        host = parsed_url.hostname
+        port = parsed_url.port
+        
+        # if we don't get a port, just use 80
+        if port == None:
+            port = 80
+
+        return host, port
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,13 +52,19 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        # returns the response code from the request
+        # the response code needs to be concatenated to an int from str when returned
+        code = (data.split(' '))[1]
+        return int(code)
 
     def get_headers(self,data):
+        # TODO
         return None
 
     def get_body(self, data):
-        return None
+        # returns the body from the request
+        body = (data.split('\r\n\r\n'))[1]
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -68,13 +85,58 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
+        # GET from URL
         code = 500
         body = ""
+        
+        host, port = self.get_host_port(url)
+        self.connect(host,port)
+        # grab the path here; in the case of no path, set the path to /
+        path = urllib.parse.urlparse(url).path
+        if path == "":
+            path = "/" 
+
+        # construct the request
+        request = "GET {path} HTTP/1.1\r\nHost: {host}:{port}\r\nAccept: */*\r\nConnection: close\r\n\r\n".format(path=path,host=host,port=port)
+        self.sendall(request)
+
+        response = self.recvall(self.socket)
+        self.close() # cLoSe yOuR sOckEtS
+
+        code = self.get_code(response)
+        body = self.get_body(response)
+        
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
+        # POST to url
         code = 500
         body = ""
+
+        # recycling this snippet of code from the GET handler
+        host, port = self.get_host_port(url)
+        self.connect(host,port)
+        # grab the path here; in the case of no path, set the path to /
+        path = urllib.parse.urlparse(url).path
+        if path == "":
+            path = "/" 
+
+        # set the body
+        if args:
+            body = urllib.parse.urlencode(args)
+        
+        content_length = len(body.encode('utf-8'))
+
+        # construct the incredibly long and ugly request gah dayum she is lengthy (not body shaming)
+        request = "POST {path} HTTP/1.1\r\nHost: {host}:{port}\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: {content_length}\r\nConnection: close\r\n\r\n{body}".format(path=path,host=host,port=port,content_length=content_length,body=body)
+        self.sendall(request)
+
+        response = self.recvall(self.socket)
+        self.close()
+
+        code = self.get_code(response)
+        body = self.get_body(response)
+
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
